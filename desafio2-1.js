384780436b45ce5cd4b893019c7cfa87e3d6f6b0
Jonathan Dominguez <jonathan-g-dominguez@outlook.es>
2022-09-11 13:32:51 -0300

    Manejo de DOM. Agregado de facturas. Suma de precios de facturas.

--- desafio2-1.js
+++ desafio2-1.js
@@ -1,3 +1,4 @@
+/*
 const USUADMIN = `Joni`;
 const PWADMIN = `123JGD`;
 let usuarioIngresado;
@@ -127,4 +128,60 @@ if(valorAdmin==true){
             break;
     }
 }
+}*/
+const USUADMIN = `Joni`;
+const PWADMIN = `123`;
+const inputUsuario = document.getElementById(`usuario`);
+const inputContrasena = document.getElementById(`contrasena`);
+const inputNumeroFactura = document.getElementById(`facturaN`);
+const inputNumeroComprobante = document.getElementById(`comprobanteN`);
+const inputCuit = document.getElementById(`cuit`);
+const inputEdificio = document.getElementById(`edificio`);
+const inputAdministracion = document.getElementById(`administracion`);
+const inputPrecio = document.getElementById(`precio`);
+const inputDescripcion = document.getElementById(`descripcion`);
+const inputFecha = document.getElementById(`fecha`);
+let facturas = [];
+
+function iniciarSesion()
+{
+    if(inputUsuario.value === USUADMIN && inputContrasena.value === PWADMIN){
+        alert(`Iniciaste sesion correctamente ${USUADMIN}`)
+        window.location.href = `./pages/facturas.html`;
+    }
+    else{
+        alert(`Error`);
+    }
 }
+function Facturacion(id,facturaN,comprobanteN,cuit,edificio,administracion,precio,descripcion,fecha)
+{
+    this.id = id;
+    this.facturaN = facturaN
+    this.comprobanteN = comprobanteN;
+    this.cuit = cuit;
+    this.edificio = edificio;
+    this.administracion = administracion;
+    this.precio = parseFloat(precio);
+    this.descripcion = descripcion;
+    this.fecha = fecha;
+}
+
+function agregarFactura()
+{
+    const nuevaFactura = new Facturacion(facturas.length,inputNumeroFactura.value,inputNumeroComprobante.value,inputCuit.value,inputEdificio.value,inputAdministracion.value,inputPrecio.value,inputDescripcion.value,inputFecha.value);
+    console.log(facturas);
+    return facturas.push(nuevaFactura);
+}
+function precioFacturas(){
+    let suma = 0;
+    let i=0;
+    for(let i=0;i<facturas.length;i++){
+        suma +=facturas[i].precio;
+        console.log(suma);
+    } 
+    let totalSumaFacturas = document.getElementById(`totalFacturas`);
+
+    totalSumaFacturas.innerHTML=`<p>$${suma}</p>`;   
+    return (suma);
+}
+
