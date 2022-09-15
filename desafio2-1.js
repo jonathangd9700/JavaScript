8cb8edc58ea9b3146885c9600581a1297635410a
Jonathan Dominguez <jonathan-g-dominguez@outlook.es>
2022-09-14 22:49:41 -0300

    Validaciones. Eliminacion de alerts. Eliminar facturas por numero de factura.

--- desafio2-1.js
+++ desafio2-1.js
@@ -1,134 +1,3 @@
-/*
-const USUADMIN = `Joni`;
-const PWADMIN = `123JGD`;
-let usuarioIngresado;
-let pwIngresada;
-let valorAdmin = false;
-let productos = [];
-let opcionElegida = "";
-let productosEliminados = [];
-
-
-function agregarProductos(){
-    let cantidadProductos = parseInt(prompt(`Cuantos productos desea agregar?`));
-    while(isNaN(cantidadProductos)===true){
-        alert(`Ingrese un valor válido`);
-        cantidadProductos = parseInt(prompt(`Cuantos productos desea agregar?`));
-    }
-    for(let i = 0; i<cantidadProductos;i++){
-let nombreProducto = prompt(`Ingrese el nombre del producto`).toLowerCase();
-while(isNaN(nombreProducto)===false){
-    alert(`Ingrese un valor valido`);
-    nombreProducto = prompt(`Ingrese el nombre del producto`).toLowerCase();
-}
-let precioProducto = parseFloat(prompt(`Ingrese el precio del producto`));
-while(isNaN(precioProducto)===true){
-    alert(`Ingrese un valor valido`);
-    precioProducto = parseFloat(prompt(`Ingrese el precio del producto`));
-}
-let stockProducto = parseInt(prompt(`Ingrese el stock del producto`));
-while(isNaN(stockProducto)===true){
-    alert(`Ingrese un valor valido`);
-    stockProducto = parseInt(prompt(`Ingrese el stock del producto`));
-}
-const ENCONTRADO = productos.find(el => {
-    return el.nombre === nombreProducto;
-})
-console.log(ENCONTRADO);
-if(ENCONTRADO === undefined){
-productos.push({nombre:nombreProducto,precio:precioProducto,stock:stockProducto});
-}
-else{
-    alert(`Este producto ya se encuentra cargado`);
-}
-}
-console.log(productos);
-}
-
-function imprimirProductos(){
-    for(let i = 0; i<productos.length;i++){
-        console.log(productos[i].nombre);
-    }
-}
-
-do{
-    usuarioIngresado = prompt(`Ingrese su nombre de usuario\nPista: ${USUADMIN}`);
-}while(usuarioIngresado != USUADMIN);
-
-if(usuarioIngresado===USUADMIN){
-    valorAdmin = true;
-    console.log(valorAdmin);
-}
-
-function eliminarProducto() {
-
-    let opcionEliminar = prompt("Ingrese que desea realizar:\n1)Eliminar ultimo producto ingresado\n2)Eliminar todos los productos\n3)Para eliminar un producto específico");
-    switch (opcionEliminar) {
-        case "1":
-            let productoEliminado = productos.pop();
-            productosEliminados.push(productoEliminado);
-            break;
-        case "2":
-            let i = 0;
-            while (i < productos.length) {
-                productosEliminados.push(productos[i]);
-                i++;
-            }
-            productos.length = 0;
-        case "3":
-            let productoElegido = prompt("Ingrese el producto a eliminar");
-            let indiceProducto =  productos.map(elemento => elemento.nombre).indexOf(productoElegido);
-            console.log(indiceProducto);
-            if(productos.indexOf(productoElegido)>-1){
-            productosEliminados.push(productoElegido);
-            productos.splice(indiceProducto,1);
-            imprimirProductos();
-        }
-            else{
-                alert(`Ese producto no se encuentra`);
-            }
-            break;
-        default:
-            alert(`Ingrese una opción válida\n"1"  "2"  "3"`)
-            break;
-    }
-
-}
-
-function verLista(){
-    let opciones = prompt(`Seleccione que lista desea ver:\n1)Productos\n2)Productos Eliminados`)
-    switch (opciones) {
-        case "1":
-            imprimirProductos();
-            break;
-        case "2":
-            console.log(productosEliminados);
-        default:
-            break;
-    }
-}
-//-----------------------------------------------------------------------------------------------------------------------
-//INTERACCION CON USUARIO
-while(opcionElegida.toLowerCase()!=`esc`){
-if(valorAdmin==true){
-    alert(`Bienvenido de nuevo "${USUADMIN}". Que deseas hacer hoy?`)
-    opcionElegida = prompt(`1)Cargar productos\n2)Modificar productos\n3)Eliminar productos\n4)Ver lista de productos\nPara finalizar escriba "ESC"`)
-    console.log(opcionElegida);
-    switch (opcionElegida) {
-        case "1":
-            agregarProductos();
-            break;
-        case "3":
-            eliminarProducto();
-            console.log(productos);
-            break;
-        case "4":
-        verLista();
-        default:
-            break;
-    }
-}
-}*/
 const USUADMIN = `Joni`;
 const PWADMIN = `123`;
 const inputUsuario = document.getElementById(`usuario`);
@@ -141,22 +10,30 @@ const inputAdministracion = document.getElementById(`administracion`);
 const inputPrecio = document.getElementById(`precio`);
 const inputDescripcion = document.getElementById(`descripcion`);
 const inputFecha = document.getElementById(`fecha`);
+let card = document.createElement(`div`);
 let facturas = [];
 
+//Funcion para iniciar sesion y avanzar hacia el simulador
 function iniciarSesion()
 {
     if(inputUsuario.value === USUADMIN && inputContrasena.value === PWADMIN){
-        alert(`Iniciaste sesion correctamente ${USUADMIN}`)
+        card.classList.add(`card`);
+        card.innerHTML = `<p>Iniciaste sesion correctamente ${USUADMIN}</p>`;
+        document.body.append(card);
         window.location.href = `./pages/facturas.html`;
     }
     else{
-        alert(`Error`);
+        card.innerHTML="";
+        card.classList.add(`card`);
+        card.innerHTML = `<p>Usuario o contraseña incorrectos</p>`;
+        document.body.append(card);
     }
 }
+//funcion constructora de los objetos facturas
 function Facturacion(id,facturaN,comprobanteN,cuit,edificio,administracion,precio,descripcion,fecha)
 {
     this.id = id;
-    this.facturaN = facturaN
+    this.facturaN = facturaN;
     this.comprobanteN = comprobanteN;
     this.cuit = cuit;
     this.edificio = edificio;
@@ -166,22 +43,64 @@ function Facturacion(id,facturaN,comprobanteN,cuit,edificio,administracion,preci
     this.fecha = fecha;
 }
 
+
+
+    //Funcion para cargar la factura
 function agregarFactura()
 {
+    card.innerHTML="";
     const nuevaFactura = new Facturacion(facturas.length,inputNumeroFactura.value,inputNumeroComprobante.value,inputCuit.value,inputEdificio.value,inputAdministracion.value,inputPrecio.value,inputDescripcion.value,inputFecha.value);
     console.log(facturas);
-    return facturas.push(nuevaFactura);
+        //Que no se duplique el numero de factura
+        const ENCONTRADO = facturas.find(el => {
+            return el.facturaN === inputNumeroFactura.value;
+        })
+    //Validaciones para permitir cargar la factura
+    if(isNaN(inputNumeroFactura.value)===false && isNaN(inputNumeroComprobante.value) === false && isNaN(inputCuit.value) === false && inputCuit.value.length === 11 && isNaN(inputPrecio.value)===false && ENCONTRADO === undefined){
+        console.log(ENCONTRADO);
+        card.classList.add(`card`);
+        card.innerHTML = `<p>Factura agregada</p>`;
+        document.body.append(card);
+        return facturas.push(nuevaFactura);
+    }
+
+        else if(ENCONTRADO!==undefined){
+            card.innerHTML="";
+            card.classList.add(`card`);
+            card.innerHTML = `<p>La factura n° <strong>${inputNumeroFactura.value}</strong> ya fue cargada</p>`;
+            document.body.append(card);
+        }
+        else if(inputCuit.value.length <11){
+            card.innerHTML="";
+            card.classList.add(`card`);
+            card.innerHTML = `<p>El cuit debe tener 11 digitos </p>`;
+            document.body.append(card);
+        }
+        else{
+            card.innerHTML="";
+            card.classList.add(`card`);
+            card.innerHTML = `<p>Ingrese un valor válido</p>`;
+            document.body.append(card);
+        }
 }
 function precioFacturas(){
-    let suma = 0;
-    let i=0;
-    for(let i=0;i<facturas.length;i++){
-        suma +=facturas[i].precio;
-        console.log(suma);
-    } 
+    const suma = facturas.reduce((acc,el)=> acc + el.precio,0)
+    console.log(suma);
     let totalSumaFacturas = document.getElementById(`totalFacturas`);
 
     totalSumaFacturas.innerHTML=`<p>$${suma}</p>`;   
     return (suma);
 }
 
+function eliminarFacturas()
+{
+    let indiceProducto =  facturas.map(elemento => elemento.facturaN).indexOf(inputNumeroFactura.value);
+    console.log(indiceProducto);
+    if(indiceProducto>-1){
+    facturas.splice(indiceProducto,1);
+    card.innerHTML="";
+    card.classList.add(`card`);
+    card.innerHTML = `<p>Factura eliminada con éxito</p>`;
+    document.body.append(card);
+}
+}
